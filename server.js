957212893dc6134f7284b7dec8cd20b368056373
Barak Horowitz <bahorcse@gmail.com>
2024-04-09 13:49:18 -0700

    added iframe to store user reviews in middle of screen

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ css_page_dir = '/mnt/c/Personals/ByteRate/css_styles/'
 js_page_dir = '/mnt/c/Personals/ByteRate/Javascript/'
 const express = require('express')
 const app = express()
-const port = 3004
+const port = 3000
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,6 +30,17 @@ app.post('/add_rest', (req, res) => {
     console.log(JSON.stringify(review_data))
 })
 
+// send html file for review page upon request
+app.get('/review_page', (req, res) => {
+    res.sendFile(html_page_dir + 'review_page.html')
+})
+
+// send javascript file for review page upon request 
+app.get('/review_page.js', (req, res) => {
+    res.sendFile(js_page_dir + 'review_page.js')
+})
+
+// listen for requests on port #3000
 app.listen(port, () => {
     console.log('ByteRate listening on port ' + port)
 })
